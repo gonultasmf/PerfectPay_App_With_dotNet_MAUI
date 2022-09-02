f5823ea910fd7162973507ddedd212cd08ab8fd7
Mustafa Gönültaş <73774639+gonultasmf@users.noreply.github.com>
2022-09-02 15:51:17 +0300

    Add files via upload

---
+++ PerfectPay/PerfectPay/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿namespace PerfectPay;
+
+public partial class MainPage : ContentPage
+{
+	decimal bill;
+	int tip;
+	int noPerson = 1;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void txtBill_Completed(object sender, EventArgs e)
+	{
+		bill = decimal.Parse(txtBill.Text);
+		CalculateTotal();
+	}
+
+	private void CalculateTotal()
+	{
+		var totalTip = (bill * tip) / 100;
+
+		var tipByPerson = (totalTip / noPerson);
+		lblTipByPerson.Text = $"{tipByPerson:C}";
+
+		var subtotal = (bill / noPerson);
+		lblSubtotal.Text = $"{subtotal:C}";
+
+		var totalByPerson = (bill + totalTip) / noPerson;
+		lblTotal.Text = $"{totalByPerson:C}";
+	}
+
+	private void sldTip_ValueChanged(object sender, ValueChangedEventArgs e)
+	{
+		tip = (int)sldTip.Value;
+		lblTip.Text = $"Tip: {tip}%";
+		CalculateTotal();
+	}
+
+	private void Button_Clicked(object sender, EventArgs e)
+	{
+		if (sender is Button)
+		{
+			var btn = (Button)sender;
+			var percentage = int.Parse(btn.Text.Replace("%", ""));
+			sldTip.Value = percentage;
+        }
+	}
+
+	private void btnMinus_Clicked(object sender, EventArgs e)
+	{
+		if (noPerson > 1)
+		{
+            noPerson--;
+            lblNoPerson.Text = noPerson.ToString();
+			CalculateTotal();
+        }
+	}
+
+	private void btnPlus_Clicked(object sender, EventArgs e)
+	{
+		noPerson++;
+		lblNoPerson.Text = noPerson.ToString();
+		CalculateTotal();
+	}
+}
+
